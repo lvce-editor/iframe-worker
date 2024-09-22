52defec2725fc1d7366e8f2607d6336ad6d686cd
Le Vivilet <levivlet@pm.me>
2024-09-22 19:45:40 +0200

    remove unused code

--- src/parts/SendPortToWebView/SendPortToWebView.ts
+++
@@ -1,7 +0,0 @@
-import * as Rpc from '../Rpc/Rpc.ts'
-
-export const sendPortToWebView = async (webviewId: string, port: MessagePort) => {
-  await Rpc.invokeAndTransfer('Transferrable.transferToRendererProcess', webviewId, port)
-  console.log('did send port to renderer process')
-  // TODO ask renderer process to transfer the port to the webview
-}
