2ecec688ef150fe04e82747579cc587910a88a80
Le Vivilet <levivlet@pm.me>
2024-10-05 11:32:25 +0200

    feature: add webview getSandbox function

--- src/parts/CommandMap/CommandMap.ts
+++ src/parts/CommandMap/CommandMap.ts
@@ -1,9 +1,13 @@
+import * as GetWebViewCsp from '../GetWebViewCsp/GetWebViewCsp.ts'
 import * as GetWebViewFrameAncestors from '../GetWebViewFrameAncestors/GetWebViewFrameAncestors.ts'
+import * as GetWebViewSandBox from '../GetWebViewSandBox/GetWebViewSandBox.ts'
 import * as Location from '../Location/Location.ts'
 
 export const commandMap = {
-  'Location.getProtocol': Location.getProtocol,
   'Location.getHost': Location.getHost,
   'Location.getOrigin': Location.getOrigin,
+  'Location.getProtocol': Location.getProtocol,
   'WebView.getFrameAncestors': GetWebViewFrameAncestors.getWebViewFrameAncestors,
+  'WebView.getSandbox': GetWebViewSandBox.getIframeSandbox,
+  'WebView.getWebViewCsp': GetWebViewCsp.getWebViewCsp,
 }
