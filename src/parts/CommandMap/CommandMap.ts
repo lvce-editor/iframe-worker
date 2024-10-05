337599f0eedfa6a99fd19630c245aa32a236ca8c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-05 10:49:33 +0200

    feature: add api functions (#9)

--- src/parts/CommandMap/CommandMap.ts
+++ src/parts/CommandMap/CommandMap.ts
@@ -1,1 +1,9 @@
-export const commandMap = {}
+import * as GetWebViewFrameAncestors from '../GetWebViewFrameAncestors/GetWebViewFrameAncestors.ts'
+import * as Location from '../Location/Location.ts'
+
+export const commandMap = {
+  'Location.getProtocol': Location.getProtocol,
+  'Location.getHost': Location.getHost,
+  'Location.getOrigin': Location.getOrigin,
+  'WebView.getFrameAncestors': GetWebViewFrameAncestors.getWebViewFrameAncestors,
+}
