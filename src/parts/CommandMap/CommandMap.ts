f2263bc5270e13f8ee3cc66a7589844befa72cf9
Le Vivilet <levivlet@pm.me>
2024-10-05 11:39:32 +0200

    feature: add getIframeSrc function

--- src/parts/CommandMap/CommandMap.ts
+++ src/parts/CommandMap/CommandMap.ts
@@ -1,3 +1,4 @@
+import * as GetIframeSrc from '../GetIframeSrc/GetIframeSrc.ts'
 import * as GetWebViewCsp from '../GetWebViewCsp/GetWebViewCsp.ts'
 import * as GetWebViewFrameAncestors from '../GetWebViewFrameAncestors/GetWebViewFrameAncestors.ts'
 import * as GetWebViewOrigin from '../GetWebViewOrigin/GetWebViewOrigin.ts'
@@ -9,6 +10,7 @@ export const commandMap = {
   'Location.getOrigin': Location.getOrigin,
   'Location.getProtocol': Location.getProtocol,
   'WebView.getFrameAncestors': GetWebViewFrameAncestors.getWebViewFrameAncestors,
+  'WebView.getIframeSrc': GetIframeSrc.getIframeSrc,
   'WebView.getOrigin': GetWebViewOrigin.getWebViewOrigin,
   'WebView.getSandbox': GetWebViewSandBox.getIframeSandbox,
   'WebView.getWebViewCsp': GetWebViewCsp.getWebViewCsp,
