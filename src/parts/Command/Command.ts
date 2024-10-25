6d26ea95173d5d234cb1f2782a0a1e481c360c8b
Le Vivilet <levivlet@pm.me>
2024-10-26 00:09:31 +0200

    feature: use command npm package

--- src/parts/Command/Command.ts
+++ src/parts/Command/Command.ts
@@ -1,9 +1,1 @@
-import * as CommandState from '../CommandState/CommandState.ts'
-
-export const execute = (command: string, ...args: any[]) => {
-  const fn = CommandState.getCommand(command)
-  if (!fn) {
-    throw new Error(`[iframe-worker] command not found ${command}`)
-  }
-  return fn(...args)
-}
+export * from '@lvce-editor/command'
