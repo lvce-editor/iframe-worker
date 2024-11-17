6267ca1c088cbff04909419b266a0757344df440
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-17 13:13:42 +0100

    feature: use explicit return types (#32)

--- src/parts/GetWebView/GetWebView.ts
+++ src/parts/GetWebView/GetWebView.ts
@@ -1,4 +1,4 @@
-export const getWebView = (webViews: any, webViewId: any) => {
+export const getWebView = (webViews: any, webViewId: any): any => {
   for (const webView of webViews) {
     if (webView.id === webViewId) {
       return webView
