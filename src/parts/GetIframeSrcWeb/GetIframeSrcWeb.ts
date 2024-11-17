8f202569cec5e5405d5c0730016b8b5b4badd916
Le Vivilet <levivlet@pm.me>
2024-11-17 13:14:27 +0100

    feature: use more explicit return types

--- src/parts/GetIframeSrcWeb/GetIframeSrcWeb.ts
+++ src/parts/GetIframeSrcWeb/GetIframeSrcWeb.ts
@@ -1,8 +1,9 @@
 import * as GetBlobUrl from '../GetBlobUrl/GetBlobUrl.ts'
 import * as GetWebViewBaseUrl from '../GetWebViewBaseUrl/GetWebViewBaseUrl.ts'
 import * as GetWebViewHtml from '../GetWebViewHtml/GetWebViewHtml.ts'
+import type { IframeSrcInfo } from '../IframeSrcInfo/IframeSrcInfo.ts'
 
-export const getIframeSrc = (webView: any, locationOrigin: string) => {
+export const getIframeSrc = (webView: any, locationOrigin: string): IframeSrcInfo | undefined => {
   const baseUrl = GetWebViewBaseUrl.getWebViewBaseUrl(webView)
   const srcHtml = GetWebViewHtml.getWebViewHtml(baseUrl, locationOrigin, webView.elements)
   if (srcHtml) {
