a327d18ad6aa0c9cd2573f6a2fe916d8c6c0e760
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-17 20:49:33 +0100

    feature: ask renderer worker for saved state (#46)

--- src/parts/ExtensionHost/ExtensionHostState.ts
+++ src/parts/ExtensionHost/ExtensionHostState.ts
@@ -1,12 +1,10 @@
 import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.ts'
-import * as ExtensionStateStorage from '../ExtensionStateStorage/ExtensionStateStorage.ts'
-
+import * as Rpc from '../Rpc/Rpc.ts'
 export const saveState = async (): Promise<any> => {
   const state = await ExtensionHostWorker.invoke('SaveState.saveState')
   return state
 }
 
 export const getSavedState = async (): Promise<any> => {
-  const value = await ExtensionStateStorage.getJson()
-  return value
+  return Rpc.invoke('WebView.getSavedState')
 }
