8edf2ed92d6f7121b3a7ea59d5b20fa1bb4545b8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-17 20:02:12 +0100

    feature: use parent process rpc to query root from shared process (#41)
    * feature: use parent process rpc to query root from shared process
    
    * add fn

--- src/parts/SharedProcess/SharedProcess.ts
+++ src/parts/SharedProcess/SharedProcess.ts
@@ -1,3 +1,5 @@
+import * as Rpc from '../Rpc/Rpc.ts'
+
 export const invoke = async (method: string, ...params: any[]): Promise<any> => {
-  // TODO
+  return Rpc.invoke('WebView.compatSharedProcessInvoke', method, ...params)
 }
