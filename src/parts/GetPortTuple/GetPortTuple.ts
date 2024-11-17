96ff7bfc62911c6733ec9138b1672a5f38f5579d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-17 20:12:13 +0100

    feature: add port tuple types (#43)

--- src/parts/GetPortTuple/GetPortTuple.ts
+++ src/parts/GetPortTuple/GetPortTuple.ts
@@ -1,4 +1,6 @@
-export const getPortTuple = (): any => {
+import type { PortTuple } from '../PortTuple/PortTuple.ts'
+
+export const getPortTuple = (): PortTuple => {
   const { port1, port2 } = new MessageChannel()
   return { port1, port2 }
 }
