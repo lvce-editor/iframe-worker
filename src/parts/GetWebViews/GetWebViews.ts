165d377298bfc9ba84814230ca0b6bc269e6e5d3
Le Vivilet <levivlet@pm.me>
2024-11-17 19:58:00 +0100

    feature: improve querying webviews

--- src/parts/GetWebViews/GetWebViews.ts
+++ src/parts/GetWebViews/GetWebViews.ts
@@ -1,23 +1,5 @@
-import * as GetWebViewsNode from '../GetWebViewsNode/GetWebViewsNode.ts'
-import * as GetWebViewsWeb from '../GetWebViewsWeb/GetWebViewsWeb.ts'
-import * as Platform from '../Platform/Platform.ts'
-import * as PlatformType from '../PlatformType/PlatformType.ts'
-
-const getWebViewsDefault = async (): Promise<readonly any[]> => {
-  switch (Platform.platform) {
-    case PlatformType.Web:
-      return GetWebViewsWeb.getWebViewsWeb()
-    case PlatformType.Test:
-      return []
-    default:
-      return GetWebViewsNode.getWebViewsNode()
-  }
-}
+import * as Rpc from '../Rpc/Rpc.ts'
 
 export const getWebViews = async (): Promise<readonly any[]> => {
-  const nodeWebViews = await getWebViewsDefault()
-  // TODO ask renderer worker for webviews
-  // const registeredWebViews = WebViews.get()
-  const allWebViews = [...nodeWebViews]
-  return allWebViews
+  return Rpc.invoke('WebView.getWebViews')
 }
--- src/parts/Listen/Listen.ts
+++ src/parts/Listen/Listen.ts
@@ -1,8 +1,10 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
+import * as Rpc from '../Rpc/Rpc.ts'
 
 export const listen = async (): Promise<void> => {
-  await WebWorkerRpcClient.create({
+  const rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
+  Rpc.setRpc(rpc)
 }
