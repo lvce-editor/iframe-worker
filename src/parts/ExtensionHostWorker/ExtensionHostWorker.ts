4677c4d95932c62571e01d2f9e97a1b2a6485875
Le Vivilet <levivlet@pm.me>
2024-11-17 20:11:20 +0100

    feature: add extension host worker invoke functions

--- src/parts/ExtensionHostWorker/ExtensionHostWorker.ts
+++ src/parts/ExtensionHostWorker/ExtensionHostWorker.ts
@@ -1,3 +1,9 @@
+import * as Rpc from '../Rpc/Rpc.ts'
+
 export const invoke = async (method: string, ...params: any[]): Promise<any> => {
-  // TODO
+  return Rpc.invoke('WebView.compatExtensionHostWorkerInvoke', method, ...params)
+}
+
+export const invokeAndTransfer = async (method: string, ...params: any[]): Promise<any> => {
+  return Rpc.invokeAndTransfer('WebView.compatExtensionHostWorkerInvokeAndTransfer', method, ...params)
 }
