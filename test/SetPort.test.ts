9a718a9a68c31e3f593506586459aada7f0f4faf
Le Vivilet <levivlet@pm.me>
2024-11-24 09:48:03 +0100

    test

--- test/SetPort.test.ts
+++ test/SetPort.test.ts
@@ -1,24 +1,30 @@
 import { beforeEach, expect, jest, test } from '@jest/globals'
+import * as Rpc from '../src/parts/Rpc/Rpc.ts'
 import * as SetPort from '../src/parts/SetPort/SetPort.ts'
 
-const RendererProcess = {
+const mockRpc = {
   invokeAndTransfer: jest.fn(),
 }
 
-jest.unstable_mockModule('../src/parts/RendererProcess/RendererProcess.ts', () => RendererProcess)
-
 beforeEach(async () => {
-  RendererProcess.invokeAndTransfer.mockReset()
+  Rpc.setRpc(mockRpc)
 })
 
 test('setPort', async () => {
   const uid = 1
-  const port = new MessagePort()
+  const { port1 } = new MessageChannel()
   const origin = 'http://localhost:3000'
   const portType = ''
 
-  await SetPort.setPort(uid, port, origin, portType)
+  await SetPort.setPort(uid, port1, origin, portType)
 
-  expect(RendererProcess.invokeAndTransfer).toHaveBeenCalledTimes(1)
-  expect(RendererProcess.invokeAndTransfer).toHaveBeenCalledWith('WebView.setPort', uid, port, origin, portType)
+  expect(mockRpc.invokeAndTransfer).toHaveBeenCalledTimes(1)
+  expect(mockRpc.invokeAndTransfer).toHaveBeenCalledWith(
+    'WebView.compatRendererProcessInvokeAndTransfer',
+    'WebView.setPort',
+    uid,
+    port1,
+    origin,
+    portType,
+  )
 })
