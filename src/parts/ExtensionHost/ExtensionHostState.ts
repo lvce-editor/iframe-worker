d0e834046543a62a894d0892d0def96da66ea6e1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-24 10:21:06 +0100

    feature: remove unused code (#66)

--- src/parts/ExtensionHost/ExtensionHostState.ts
+++ src/parts/ExtensionHost/ExtensionHostState.ts
@@ -1,5 +1,6 @@
 import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.ts'
 import * as Rpc from '../Rpc/Rpc.ts'
+
 export const saveState = async (): Promise<any> => {
   const state = await ExtensionHostWorker.invoke('SaveState.saveState')
   return state
