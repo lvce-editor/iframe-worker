63d2f7fc4ab7dbf2be468641932ba15a22997406
Le Vivilet <levivlet@pm.me>
2024-12-20 22:55:45 +0100

    feature: adjust baseUrl when baseUrl starts with location origin

--- test/GetWebViewBaseUrl.test.ts
+++ test/GetWebViewBaseUrl.test.ts
@@ -1,30 +1,32 @@
 import { expect, test } from '@jest/globals'
 import * as GetWebViewBaseUrl from '../src/parts/GetWebViewBaseUrl/GetWebViewBaseUrl.ts'
 
+const locationOrigin = ''
+
 test('remotePath with index.html', () => {
   const webView = {
     remotePath: '/test/path/index.html',
   }
-  expect(GetWebViewBaseUrl.getWebViewBaseUrl(webView)).toBe('/test/path')
+  expect(GetWebViewBaseUrl.getWebViewBaseUrl(webView, locationOrigin)).toBe('/test/path')
 })
 
 test('remotePath without index.html', () => {
   const webView = {
     remotePath: '/test/path',
   }
-  expect(GetWebViewBaseUrl.getWebViewBaseUrl(webView)).toBe('/test/path')
+  expect(GetWebViewBaseUrl.getWebViewBaseUrl(webView, locationOrigin)).toBe('/test/path')
 })
 
 test('path with index.html', () => {
   const webView = {
     path: '/local/path/index.html',
   }
-  expect(GetWebViewBaseUrl.getWebViewBaseUrl(webView)).toBe('/local/path')
+  expect(GetWebViewBaseUrl.getWebViewBaseUrl(webView, locationOrigin)).toBe('/local/path')
 })
 
 test('path without index.html', () => {
   const webView = {
     path: '/local/path',
   }
-  expect(GetWebViewBaseUrl.getWebViewBaseUrl(webView)).toBe('/local/path')
+  expect(GetWebViewBaseUrl.getWebViewBaseUrl(webView, locationOrigin)).toBe('/local/path')
 })
