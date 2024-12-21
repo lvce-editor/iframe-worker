9bb3ade54d8874f65c901af0301d4798beaefd58
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-21 23:53:17 +0100

    feature: add shared process test (#96)

---
+++ test/SharedProcess.test.ts
@@ -0,0 +1,23 @@
+import { beforeEach, expect, jest, test } from '@jest/globals'
+
+const Rpc = {
+  invoke: jest.fn(),
+}
+
+jest.unstable_mockModule('../src/parts/Rpc/Rpc.ts', () => Rpc)
+
+const SharedProcess = await import('../src/parts/SharedProcess/SharedProcess.ts')
+
+beforeEach(() => {
+  Rpc.invoke.mockReset()
+})
+
+test('invoke', async () => {
+  await SharedProcess.invoke('test.method', 'arg1', 'arg2')
+  expect(Rpc.invoke).toHaveBeenCalledWith('WebView.compatSharedProcessInvoke', 'test.method', 'arg1', 'arg2')
+})
+
+test('error case', async () => {
+  Rpc.invoke.mockImplementation(() => Promise.reject(new Error('test error')))
+  await expect(SharedProcess.invoke('test.method')).rejects.toThrow('test error')
+})
