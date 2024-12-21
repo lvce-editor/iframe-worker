e01a46e5a917d68970f0178d9344c270e9e560f2
Le Vivilet <levivlet@pm.me>
2024-12-21 16:05:05 +0100

    return

--- src/parts/GetCredentialLess/GetCredentialLess.ts
+++ src/parts/GetCredentialLess/GetCredentialLess.ts
@@ -1,4 +1,4 @@
-export const getCredentialLess = (locationHost: string) => {
+export const getCredentialLess = (locationHost: string): boolean => {
   if (locationHost.startsWith('localhost:')) {
     // disabled to improve performance and make testing easier
     return false
