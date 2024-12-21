2b731fb699e5e8efa3e60ab9307735bcafe02bad
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-21 09:14:21 +0100

    feature: move webview protocol module into separate file (#78)

--- src/parts/WebViewProtocol/WebViewProtocol.ts
+++ src/parts/WebViewProtocol/WebViewProtocol.ts
@@ -1,20 +1,5 @@
+import * as GetWebViewProtocolModule from '../GetWebViewProtocolModule/GetWebViewProtocolModule.ts'
 import * as Platform from '../Platform/Platform.ts'
-import * as PlatformType from '../PlatformType/PlatformType.ts'
-import * as WebViewProtocolElectron from '../WebViewProtocolElectron/WebViewProtocolElectron.ts'
-import * as WebViewProtocolRemote from '../WebViewProtocolRemote/WebViewProtocolRemote.ts'
-import * as WebViewProtocolWeb from '../WebViewProtocolWeb/WebViewProtocolWeb.ts'
-
-const getModule = (platform: number): any => {
-  switch (platform) {
-    case PlatformType.Remote:
-      return WebViewProtocolRemote.register
-    case PlatformType.Electron:
-      return WebViewProtocolElectron.register
-    case PlatformType.Web:
-    default:
-      return WebViewProtocolWeb.register
-  }
-}
 
 export const register = async (
   previewServerId: number,
@@ -24,6 +9,6 @@ export const register = async (
   csp: string,
   iframeContent: string,
 ): Promise<void> => {
-  const fn = getModule(Platform.platform)
+  const fn = GetWebViewProtocolModule.getModule(Platform.platform)
   return fn(previewServerId, webViewPort, frameAncestors, webViewRoot, csp, iframeContent)
 }
