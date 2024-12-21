85020f54a7edb2182aadccfda4fa2bc1c58a7049
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-21 23:35:52 +0100

    feature: add more main tests (#88)
    * feature: add more main tests
    
    * test

--- test/Main.test.ts
+++ test/Main.test.ts
@@ -1,6 +1,24 @@
-import { expect, test } from '@jest/globals'
-import * as Main from '../src/parts/Main/Main.ts'
+import { beforeEach, expect, jest, test } from '@jest/globals'
 
-test('main', () => {
-  expect(typeof Main.main).toBe('function')
+const Listen = {
+  listen: jest.fn(),
+}
+
+jest.unstable_mockModule('../src/parts/Listen/Listen.ts', () => Listen)
+
+const Main = await import('../src/parts/Main/Main.ts')
+
+beforeEach(() => {
+  Listen.listen.mockReset()
+})
+
+test('main', async () => {
+  await Main.main()
+  expect(Listen.listen).toHaveBeenCalledTimes(1)
+})
+
+test('error case', async () => {
+  // @ts-ignore
+  Listen.listen.mockRejectedValue(new Error('test error'))
+  await expect(Main.main()).rejects.toThrow('test error')
 })
