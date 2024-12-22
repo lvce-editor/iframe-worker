454d8c9d12d971dd7d23680aeff0e605ae6a152f
Le Vivilet <levivlet@pm.me>
2024-12-22 16:13:48 +0100

    memory

--- packages/memory/src/measureMemory.ts
+++ packages/memory/src/measureMemory.ts
@@ -1,1 +1,12 @@
-export {}
+import { measureMemory } from '@lvce-editor/measure-memory'
+import { playwrightPath, threshold, workerPath } from './config.ts'
+
+const main = async () => {
+  await measureMemory({
+    playwrightPath,
+    workerPath,
+    threshold,
+  })
+}
+
+main()
