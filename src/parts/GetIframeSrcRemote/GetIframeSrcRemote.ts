475ce898d1319a168eae732d02b3267998635e06
Le Vivilet <levivlet@pm.me>
2024-12-22 11:41:26 +0100

    fix: try to use file uris to support windows

--- src/parts/GetIframeSrcRemote/GetIframeSrcRemote.ts
+++ src/parts/GetIframeSrcRemote/GetIframeSrcRemote.ts
@@ -1,9 +1,8 @@
-import type { IframeSrcInfo } from '../IframeSrcInfo/IframeSrcInfo.ts'
 import * as CreateLocalHostUrl from '../CreateLocalHostUrl/CreateLocalHostUrl.ts'
 import * as GetWebView from '../GetWebView/GetWebView.ts'
 import * as GetWebViewHtml from '../GetWebViewHtml/GetWebViewHtml.ts'
 import * as GetWebViewUri from '../GetWebViewUri/GetWebViewUri.ts'
-import * as IsWindowsPath from '../IsWindowsPath/IsWindowsPath.ts'
+import type { IframeSrcInfo } from '../IframeSrcInfo/IframeSrcInfo.ts'
 import * as Platform from '../Platform/Platform.ts'
 import * as PlatformType from '../PlatformType/PlatformType.ts'
 import * as Scheme from '../Scheme/Scheme.ts'
@@ -34,15 +33,7 @@ export const getIframeSrcRemote = (
     iframeSrc = `${Scheme.WebView}://-${relativePath}/`
     // TODO
   } else if (platform === PlatformType.Remote) {
-    const relativePath = new URL(webViewUri).pathname.replace('/index.html', '')
-    if (webViewUri.startsWith('file://')) {
-      // ignore
-      webViewRoot = webViewUri.slice('file://'.length).replace('/index.html', '')
-    } else if (IsWindowsPath.isWindowsPath(relativePath)) {
-      webViewRoot = relativePath
-    } else {
-      webViewRoot = root + relativePath
-    }
+    webViewRoot = webView.uri
     iframeSrc = CreateLocalHostUrl.createLocalHostUrl(locationProtocol, locationHost, isGitpod, webViewPort)
   }
   let iframeContent = GetWebViewHtml.getWebViewHtml('', '', webView.elements, assetDir)
