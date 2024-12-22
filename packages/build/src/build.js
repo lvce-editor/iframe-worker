e29c93f692c6eec887ee45e49aac879224323a2c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-22 16:17:52 +0100

    feature: convert to monorepo (#108)
    * feature: convert to monorepo
    
    * fix
    
    * memory
    
    * lower max memory

--- packages/build/src/build.js
+++ packages/build/src/build.js
@@ -56,7 +56,7 @@ await bundleJs()
 
 const version = await getVersion()
 
-const packageJson = await readJson(join(root, 'packages', 'text-search-worker', 'package.json'))
+const packageJson = await readJson(join(root, 'packages', 'iframe-worker', 'package.json'))
 
 delete packageJson.scripts
 delete packageJson.devDependencies
@@ -66,7 +66,7 @@ delete packageJson.xo
 delete packageJson.directories
 delete packageJson.nodemonConfig
 packageJson.version = version
-packageJson.main = 'dist/textSearchWorkerMain.js'
+packageJson.main = 'dist/iframeWorkerMain.js'
 
 await writeJson(join(dist, 'package.json'), packageJson)
 
