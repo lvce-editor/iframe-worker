5a3f4b7210e3a44f79dd280ece0d151ad15e924f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-23 14:05:15 +0100

    feature: update webviewOrigin for electron (#117)

--- packages/iframe-worker/test/GetWebViewOrigin.test.ts
+++ packages/iframe-worker/test/GetWebViewOrigin.test.ts
@@ -3,23 +3,24 @@ import * as GetWebViewOrigin from '../src/parts/GetWebViewOrigin/GetWebViewOrigi
 import * as PlatformType from '../src/parts/PlatformType/PlatformType.ts'
 
 const webViewScheme = 'lvce-oss-webview'
+const webViewId = 'test'
 
 test('electron platform', () => {
   const webViewPort = 3000
-  expect(GetWebViewOrigin.getWebViewOrigin(webViewPort, PlatformType.Electron, webViewScheme)).toBe('lvce-oss-webview://-/')
+  expect(GetWebViewOrigin.getWebViewOrigin(webViewPort, PlatformType.Electron, webViewScheme, webViewId)).toBe('lvce-oss-webview://test')
 })
 
 test('remote platform', () => {
   const webViewPort = 3000
-  expect(GetWebViewOrigin.getWebViewOrigin(webViewPort, PlatformType.Remote, webViewScheme)).toBe('http://localhost:3000')
+  expect(GetWebViewOrigin.getWebViewOrigin(webViewPort, PlatformType.Remote, webViewScheme, webViewId)).toBe('http://localhost:3000')
 })
 
 test('web platform', () => {
   const webViewPort = 3000
-  expect(GetWebViewOrigin.getWebViewOrigin(webViewPort, PlatformType.Web, webViewScheme)).toBe('*')
+  expect(GetWebViewOrigin.getWebViewOrigin(webViewPort, PlatformType.Web, webViewScheme, webViewId)).toBe('*')
 })
 
 test('test platform', () => {
   const webViewPort = 3000
-  expect(GetWebViewOrigin.getWebViewOrigin(webViewPort, PlatformType.Test, webViewScheme)).toBe('*')
+  expect(GetWebViewOrigin.getWebViewOrigin(webViewPort, PlatformType.Test, webViewScheme, webViewId)).toBe('*')
 })
