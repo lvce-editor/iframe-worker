64cc60e532b771c28779723cb84ca67d15b30084
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-29 13:34:12 +0100

    feature: add create3 function to create webview from extension host worker (#128)

--- packages/iframe-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/iframe-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,7 @@
 import * as Create2 from '../Create2/Create2.ts'
+import * as Create3 from '../Create3/Create3.ts'
 
 export const commandMap = {
   'WebView.create2': Create2.create2,
+  'WebView.create3': Create3.create3,
 }
