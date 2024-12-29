5b5922c0249e5e4c0a97cc545d1a82b68b78a694
Le Vivilet <levivlet@pm.me>
2024-12-29 22:05:19 +0100

    fix: iframe remote url when running on localhost

--- packages/iframe-worker/src/parts/GetIframeSrcRemoteBaseUrl/GetIframeSrcRemoteBaseUrl.ts
+++ packages/iframe-worker/src/parts/GetIframeSrcRemoteBaseUrl/GetIframeSrcRemoteBaseUrl.ts
@@ -6,12 +6,12 @@ export const getIframeSrcRemoteBaseUrl = (
   platform: number,
   webViewId: string,
   useNewWebViewHandler: boolean,
+  root: string,
 ): string => {
   if (webViewRoot && (webViewRoot.startsWith('http:') || webViewRoot.startsWith('https:'))) {
     if (webViewRoot.startsWith(locationOrigin)) {
       const baseUrl = webViewRoot.slice(locationOrigin.length)
-
-      return baseUrl
+      return `/remote${root}${baseUrl}`
     }
     return webViewRoot
   }
