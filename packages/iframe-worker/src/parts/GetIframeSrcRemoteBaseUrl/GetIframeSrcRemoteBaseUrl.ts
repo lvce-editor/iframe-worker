3801bc98cac7dec4a478d60c53a5e4cbce02b436
Le Vivilet <levivlet@pm.me>
2024-12-29 14:59:47 +0100

    fix

--- packages/iframe-worker/src/parts/GetIframeSrcRemoteBaseUrl/GetIframeSrcRemoteBaseUrl.ts
+++ packages/iframe-worker/src/parts/GetIframeSrcRemoteBaseUrl/GetIframeSrcRemoteBaseUrl.ts
@@ -1,5 +1,5 @@
 export const getIframeSrcRemoteBaseUrl = (webViewRoot: string, locationOrigin: string): string => {
-  if (webViewRoot.startsWith('http:') || webViewRoot.startsWith('https:')) {
+  if ((webViewRoot && webViewRoot.startsWith('http:')) || webViewRoot.startsWith('https:')) {
     if (webViewRoot.startsWith(locationOrigin)) {
       return webViewRoot.slice(locationOrigin.length)
     }
