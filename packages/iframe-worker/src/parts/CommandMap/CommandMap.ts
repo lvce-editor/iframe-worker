8e3cf99638358d13d86ead80a4fd4fa1e6f433ff
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-29 12:57:46 +0100

    Discard changes to packages/iframe-worker/src/parts/CommandMap/CommandMap.ts

--- packages/iframe-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/iframe-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,7 +1,5 @@
 import * as Create2 from '../Create2/Create2.ts'
-import * as Create3 from '../Create3/Create3.ts'
 
 export const commandMap = {
   'WebView.create2': Create2.create2,
-  'WebView.create3': Create3.create3,
 }
