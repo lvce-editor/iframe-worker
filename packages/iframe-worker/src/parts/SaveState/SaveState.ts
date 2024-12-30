6c34c584c0a23afdf4e7754e7eacebfdca0f6a38
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-30 21:58:13 +0100

    feature: add function to save iframe state (#160)
    * feature: add function to save iframe state
    
    * coverage
    
    * fix: restoring state
    
    * coverage
    
    * lint

---
+++ packages/iframe-worker/src/parts/SaveState/SaveState.ts
@@ -0,0 +1,20 @@
+import * as RpcState from '../RpcState/RpcState.ts'
+
+export const saveState = async (): Promise<readonly any[]> => {
+  const all = RpcState.getAll()
+  const serialized = []
+  for (const value of Object.values(all)) {
+    try {
+      const savedState = await value.rpc.invoke('WebView.saveState', value.portId)
+      serialized.push({
+        key: value.webViewId,
+        value: savedState,
+      })
+    } catch (error) {
+      console.log(error)
+      // TODO maybe log the error
+      // ignore
+    }
+  }
+  return serialized
+}
