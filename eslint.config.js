2bde0864955b4bb6451522d8fa267508bc2f508c
Le Vivilet <levivlet@pm.me>
2025-01-16 22:27:16 +0100

    feature: simplify eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,47 +3,20 @@ import config from '@lvce-editor/eslint-config'
 export default [
   ...config,
   {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    ignores: ['src/iframeWorkerMain.ts'],
-  },
-  {
     ignores: [
+      'src/iframeWorkerMain.ts',
+      'packages/iframe-worker/src/iframeWorkerMain.ts',
       '**/server/**',
       '**/e2e/**',
       '**/memory/**',
       '**/test-integration/**',
       '**/test-integration-util/**',
-      'packages/iframe-worker/src/iframeWorkerMain.ts',
     ],
   },
   {
     rules: {
-      '@typescript-eslint/explicit-function-return-type': 'error',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/ban-ts-comment': 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'off',
-      '@typescript-eslint/no-unsafe-assignment': 'off',
-      '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-return': 'off',
-      '@typescript-eslint/no-unsafe-argument': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/require-await': 'off',
-      '@typescript-eslint/restrict-template-expressions': 'off',
-      '@typescript-eslint/no-deprecated': 'off',
-      '@typescript-eslint/no-unnecessary-condition': 'off',
-      '@typescript-eslint/no-unnecessary-template-expression': 'off',
-      'n/no-unpublished-import': 'off',
-      'n/no-unsupported-features/node-builtins': 'off',
-      'n/no-extraneous-import': 'off',
-
       '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   },
 ]
