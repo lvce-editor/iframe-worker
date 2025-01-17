c324d1dbfba8c578023bf1d23482d0b297657d5d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-17 20:26:09 +0100

    update dependencies (#176)

--- packages/iframe-worker/src/parts/GetWebViewHtml/GetWebViewHtml.ts
+++ packages/iframe-worker/src/parts/GetWebViewHtml/GetWebViewHtml.ts
@@ -5,13 +5,21 @@ export const getWebViewHtml = (baseUrl: string, locationOrigin: string, elements
   const middle: string[] = []
   middle.push('<meta charset="utf-8">')
   for (const element of elements) {
-    if (element.type === 'title') {
-      middle.push(`<title>${element.value}</title>`)
-    } else if (element.type === 'script') {
-      middle.push(`<script type="module" src="${locationOrigin}${assetDir}/js/preview-injected.js"></script>`)
-      middle.push(`<script type="module" src="${locationOrigin}${baseUrl}/${element.path}"></script>`)
-    } else if (element.type === 'css') {
-      middle.push(`<link rel="stylesheet" href="${locationOrigin}${baseUrl}/${element.path}" />`)
+    switch (element.type) {
+      case 'title':
+        middle.push(`<title>${element.value}</title>`)
+
+        break
+      case 'script':
+        middle.push(`<script type="module" src="${locationOrigin}${assetDir}/js/preview-injected.js"></script>`)
+        middle.push(`<script type="module" src="${locationOrigin}${baseUrl}/${element.path}"></script>`)
+
+        break
+      case 'css':
+        middle.push(`<link rel="stylesheet" href="${locationOrigin}${baseUrl}/${element.path}" />`)
+        break
+      default:
+        break
     }
   }
   const middleHtml = middle.join('\n    ')
