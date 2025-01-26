ffee2290e648c9910f2ba9520049a770e1c16220
Le Vivilet <levivlet@pm.me>
2025-01-26 18:52:56 +0100

    feature: use readonly parameters

--- packages/iframe-worker/src/parts/CreateSecondaryWebViewConnection/CreateSecondaryWebViewConnection.ts
+++ packages/iframe-worker/src/parts/CreateSecondaryWebViewConnection/CreateSecondaryWebViewConnection.ts
@@ -1,5 +1,6 @@
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
+// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
 export const createSecondaryWebViewConnection = async (uid: number, origin: string, port: MessagePort): Promise<void> => {
   const portType = 'application'
   await RendererWorker.invokeAndTransfer('WebView.setPort', uid, port, origin, portType)
--- packages/iframe-worker/src/parts/RendererWorker/RendererWorker.ts
+++ packages/iframe-worker/src/parts/RendererWorker/RendererWorker.ts
@@ -1,9 +1,9 @@
 import * as Rpc from '../Rpc/Rpc.ts'
 
-export const invoke = async (method: string, ...params: any[]): Promise<any> => {
+export const invoke = async (method: string, ...params: readonly any[]): Promise<any> => {
   return Rpc.invoke('WebView.compatRendererWorkerInvoke', method, ...params)
 }
 
-export const invokeAndTransfer = async (method: string, ...params: any[]): Promise<any> => {
+export const invokeAndTransfer = async (method: string, ...params: readonly any[]): Promise<any> => {
   return Rpc.invokeAndTransfer('WebView.compatRendererWorkerInvokeAndTransfer', method, ...params)
 }
