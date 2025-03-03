6dc67813aee2e96ccd7b50dc02f9d54a341999b1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-03 22:05:50 +0100

    fix: serialize webview info (#216)

--- packages/iframe-worker/src/parts/GetWebViewInfo/GetWebViewInfo.ts
+++ packages/iframe-worker/src/parts/GetWebViewInfo/GetWebViewInfo.ts
@@ -1,10 +1,14 @@
+import type { WebViewInfo } from '../WebViewInfo/WebViewInfo.ts'
 import * as RpcState from '../RpcState/RpcState.ts'
 
-export const getWebViewInfo = (webViewId: string): any => {
+export const getWebViewInfo = (webViewId: string): WebViewInfo | undefined => {
   const rpcs = RpcState.getAll()
   for (const value of Object.values(rpcs)) {
     if (value.webViewId === webViewId) {
-      return value
+      return {
+        uid: value.webViewUid,
+        origin: value.origin,
+      }
     }
   }
   return undefined
