6de3d9ce5ce504dc1d090b10f3fae5d479e8b0be
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-09 15:15:55 +0100

    feature: add remote url test (#228)
    * feature: add remote url test
    
    * test

---
+++ packages/iframe-worker/test/GetRemoteUrl.test.ts
@@ -0,0 +1,64 @@
+import { expect, test } from '@jest/globals'
+import * as GetRemoteUrl from '../src/parts/GetRemoteUrl/GetRemoteUrl.ts'
+import * as PlatformState from '../src/parts/PlatformState/PlatformState.ts'
+import * as PlatformType from '../src/parts/PlatformType/PlatformType.ts'
+
+test('remote platform - absolute path', async () => {
+  PlatformState.setPlatform(PlatformType.Remote)
+  const options = {
+    uri: '/test/path',
+    id: 1,
+  }
+  const result = await GetRemoteUrl.getRemoteUrl(options)
+  expect(result).toBe('/remote/test/path')
+})
+
+test('remote platform - relative path', async () => {
+  PlatformState.setPlatform(PlatformType.Remote)
+  const options = {
+    uri: 'test/path',
+    id: 1,
+  }
+  const result = await GetRemoteUrl.getRemoteUrl(options)
+  expect(result).toBe('/remote/test/path')
+})
+
+test('electron platform - absolute path', async () => {
+  PlatformState.setPlatform(PlatformType.Electron)
+  const options = {
+    uri: '/test/path',
+    id: 1,
+  }
+  const result = await GetRemoteUrl.getRemoteUrl(options)
+  expect(result).toBe('/remote/test/path')
+})
+
+test('electron platform - relative path', async () => {
+  PlatformState.setPlatform(PlatformType.Electron)
+  const options = {
+    uri: 'test/path',
+    id: 1,
+  }
+  const result = await GetRemoteUrl.getRemoteUrl(options)
+  expect(result).toBe('/remote/test/path')
+})
+
+test.skip('web platform', async () => {
+  PlatformState.setPlatform(PlatformType.Web)
+  const options = {
+    uri: 'test/path',
+    id: 1,
+  }
+  const result = await GetRemoteUrl.getRemoteUrl(options)
+  expect(result).toBe('/remote/test/path')
+})
+
+test.skip('uri with protocol', async () => {
+  PlatformState.setPlatform(PlatformType.Remote)
+  const options = {
+    uri: 'file:///test/path',
+    id: 1,
+  }
+  const result = await GetRemoteUrl.getRemoteUrl(options)
+  expect(result).toBe('/remote/test/path')
+})
