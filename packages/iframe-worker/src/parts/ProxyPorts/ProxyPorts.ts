ec999e9b08dc312554d1182b07db0fcd9b60e03f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-10 22:40:22 +0100

    feature: add proxyPorts function (#234)

---
+++ packages/iframe-worker/src/parts/ProxyPorts/ProxyPorts.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+export const proxyPorts = (port1: MessagePort, port2: MessagePort): void => {
+  port1.addEventListener('message', (event: any) => {
+    port2.postMessage(event.data)
+  })
+
+  port2.addEventListener('message', (event: any) => {
+    port1.postMessage(event.data)
+  })
+}
