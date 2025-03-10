a5723d19df139dff2fa2fe484605f71f8684580c
Le Vivilet <levivlet@pm.me>
2025-03-10 22:22:08 +0100

    feature: add webview interceptor state

---
+++ packages/iframe-worker/src/parts/WebViewInterceptorState/WebViewInterceptorState.ts
@@ -0,0 +1,14 @@
+const interceptors = Object.create(null)
+
+// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+export const add = (id: number, port: MessagePort): void => {
+  interceptors[id] = port
+}
+
+export const remove = (id: number): void => {
+  delete interceptors[id]
+}
+
+export const getAll = (): readonly any[] => {
+  return Object.values(interceptors)
+}
