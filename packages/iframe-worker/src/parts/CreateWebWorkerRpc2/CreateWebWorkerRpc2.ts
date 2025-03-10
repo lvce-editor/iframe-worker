3326c41ade07d1b66230804b21b383466471f164
Le Vivilet <levivlet@pm.me>
2025-03-10 22:56:27 +0100

    feature: intercept webview messages and send them to interceptors

--- packages/iframe-worker/src/parts/CreateWebWorkerRpc2/CreateWebWorkerRpc2.ts
+++ packages/iframe-worker/src/parts/CreateWebWorkerRpc2/CreateWebWorkerRpc2.ts
@@ -1,8 +1,10 @@
 import type { WebView } from '../WebView/WebView.ts'
 import * as CreateSecondaryWebViewConnection from '../CreateSecondaryWebViewConnection/CreateSecondaryWebViewConnection.ts'
 import * as CreateWebViewConnection from '../CreateWebViewConnection/CreateWebViewConnection.ts'
-import * as GetPortTuple from '../GetPortTuple/GetPortTuple.ts'
+import * as GetPortQuadruple from '../GetPortQuadruple/GetPortQuadruple.ts'
 import * as GetWebViewWorkerRpc2 from '../GetWebViewWorkerRpc2/GetWebViewWorkerRpc2.ts'
+import * as NotifyInterceptors from '../NotifyInterceptors/NotifyInterceptors.ts'
+import * as ProxyPorts from '../ProxyPorts/ProxyPorts.ts'
 import * as RpcState from '../RpcState/RpcState.ts'
 
 export const createWebWorkerRpc2 = async (
@@ -27,8 +29,23 @@ export const createWebWorkerRpc2 = async (
   // TODO this connection might not be needed
   await CreateWebViewConnection.createWebViewConnection(webViewUid, origin)
 
-  const { port1, port2 } = GetPortTuple.getPortTuple()
+  const { port1, port2, port3, port4 } = GetPortQuadruple.getPortQuadruple()
   await CreateSecondaryWebViewConnection.createSecondaryWebViewConnection(webViewUid, origin, port1)
-  await rpc.invokeAndTransfer('_WebView.setPort', portId, port2)
+
+  // TODO remove message listeners on dispose
+  port2.addEventListener('message', (event: any): void => {
+    NotifyInterceptors.notifyInterceptors(event.data)
+  })
+
+  port3.addEventListener('message', (event: any): void => {
+    NotifyInterceptors.notifyInterceptors(event.data)
+  })
+  ProxyPorts.proxyPorts(port2, port3)
+  port1.start()
+  port2.start()
+  port3.start()
+  port4.start()
+
+  await rpc.invokeAndTransfer('_WebView.setPort', portId, port4)
   await rpc.invoke('_WebView.create', { id: portId, savedState, webViewId: webView.id, uri })
 }
