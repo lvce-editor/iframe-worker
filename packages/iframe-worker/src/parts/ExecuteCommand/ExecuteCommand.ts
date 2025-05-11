07c409320833e40696fd7ca747f04677ef99e6b2
Le Vivilet <levivlet@pm.me>
2025-05-12 00:59:40 +0200

    feature: rename external command

--- packages/iframe-worker/src/parts/ExecuteCommand/ExecuteCommand.ts
+++ packages/iframe-worker/src/parts/ExecuteCommand/ExecuteCommand.ts
@@ -1,5 +1,5 @@
 import * as ParentRpc from '../Rpc/Rpc.ts'
 
-export const executeCommand=(method:string, ...params:readonly any[]):Promise<any>=>{
-  return ParentRpc.invoke(method, ...params)
+export const executeCommand = (method: string, ...params: readonly any[]): Promise<any> => {
+  return ParentRpc.invoke('ExecuteExternalCommand.executeExternalCommand', method, ...params)
 }
