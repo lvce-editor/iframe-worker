d4c307bc2d2a43a72cff0e31faeb95ecd5af2d63
Le Vivilet <levivlet@pm.me>
2025-05-12 00:56:51 +0200

    feature: add executeExternalCommand

--- packages/iframe-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/iframe-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,6 @@
 import * as Create2 from '../Create2/Create2.ts'
 import * as Create3 from '../Create3/Create3.ts'
+import * as ExecuteCommand from '../ExecuteCommand/ExecuteCommand.ts'
 import * as GetSecret from '../GetSecret/GetSecret.ts'
 import * as GetWebViewInfo from '../GetWebViewInfo/GetWebViewInfo.ts'
 import * as SaveState from '../SaveState/SaveState.ts'
@@ -13,4 +14,5 @@ export const commandMap = {
   'WebView.getWebViewInfo': GetWebViewInfo.getWebViewInfo,
   'WebView.registerInterceptor': WebViewInterceptor.registerInterceptor,
   'WebView.unregisterInterceptor': WebViewInterceptor.unregisterInterceptor,
+  'WebView.executeExternalCommand': ExecuteCommand.executeCommand,
 }
---
+++ packages/iframe-worker/src/parts/ExecuteCommand/ExecuteCommand.ts
@@ -0,0 +1,5 @@
+import * as ParentRpc from '../Rpc/Rpc.ts'
+
+export const executeCommand=(method:string, ...params:readonly any[]):Promise<any>=>{
+  return ParentRpc.invoke(method, ...params)
+}
