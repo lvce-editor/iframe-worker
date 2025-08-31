5722e386a8b7c9b98ba7b19d54540ee726d106ea
Le Vivilet <levivlet@pm.me>
2025-08-31 19:29:57 +0200

    sc

--- packages/iframe-worker/src/parts/RendererWorker/RendererWorker.ts
+++ packages/iframe-worker/src/parts/RendererWorker/RendererWorker.ts
@@ -1,9 +1,11 @@
 import * as Rpc from '../Rpc/Rpc.ts'
 
 export const invoke = async (method: string, ...params: readonly any[]): Promise<any> => {
+  // @ts-ignore
   return Rpc.invoke('WebView.compatRendererWorkerInvoke', method, ...params)
 }
 
 export const invokeAndTransfer = async (method: string, ...params: readonly any[]): Promise<any> => {
+  // @ts-ignore
   return Rpc.invokeAndTransfer('WebView.compatRendererWorkerInvokeAndTransfer', method, ...params)
 }
