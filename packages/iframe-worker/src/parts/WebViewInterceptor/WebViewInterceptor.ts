79a56c7667bd4d15de95830608e5a405a2b95d68
Le Vivilet <levivlet@pm.me>
2025-08-31 19:09:25 +0200

    lint

--- packages/iframe-worker/src/parts/WebViewInterceptor/WebViewInterceptor.ts
+++ packages/iframe-worker/src/parts/WebViewInterceptor/WebViewInterceptor.ts
@@ -1,6 +1,6 @@
 import * as WebViewInterceptorState from '../WebViewInterceptorState/WebViewInterceptorState.ts'
 
-// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+ 
 export const registerInterceptor = async (id: number, port: MessagePort): Promise<void> => {
   WebViewInterceptorState.add(id, port)
 }
--- packages/iframe-worker/src/parts/WebViewInterceptorState/WebViewInterceptorState.ts
+++ packages/iframe-worker/src/parts/WebViewInterceptorState/WebViewInterceptorState.ts
@@ -1,6 +1,6 @@
 const interceptors = Object.create(null)
 
-// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+ 
 export const add = (id: number, port: MessagePort): void => {
   interceptors[id] = port
 }
