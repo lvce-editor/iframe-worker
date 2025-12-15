99a9a96a9a4ae02dcff5c1525b178222acd85725
Le Vivilet <levivilet@pm.me>
2025-12-15 21:13:13 +0100

    feature: update eslint config (#350)

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,14 @@
 import * as config from '@lvce-editor/eslint-config'
 import * as actions from '@lvce-editor/eslint-plugin-github-actions'
+import * as tsconfig from '@lvce-editor/eslint-plugin-tsconfig'
 
 export default [
   ...config.default,
   ...actions.default,
+  ...tsconfig.default,
   {
     ignores: [
       '**/server/**',
-      '**/e2e/**',
       '**/memory/**',
       '**/test-integration/**',
       '**/test-integration-util/**',
