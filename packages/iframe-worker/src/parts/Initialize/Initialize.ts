297e25cd44f9e7abe4dff040f9d8a15e9742fc8d
Le Vivilet <levivilet@pm.me>
2025-12-15 21:44:18 +0100

    import

--- packages/iframe-worker/src/parts/Initialize/Initialize.ts
+++ packages/iframe-worker/src/parts/Initialize/Initialize.ts
@@ -1,12 +1,12 @@
 import { TransferMessagePortRpcParent } from '@lvce-editor/rpc'
-import * as RpcRegistry from '../RpcRegistry/RpcRegistry.ts'
+import { ExtensionHost, RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const initialize = async (): Promise<void> => {
   const rpc = await TransferMessagePortRpcParent.create({
     commandMap: {},
     async send(port) {
-      await RpcRegistry.RendererWorker.sendMessagePortToExtensionHostWorker(port, 0)
+      await RendererWorker.sendMessagePortToExtensionHostWorker(port, 0)
     },
   })
-  RpcRegistry.ExtensionHost.set(rpc)
+  ExtensionHost.set(rpc)
 }
