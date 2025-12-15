cbe074189ed42f7adeb7e3bf80411d843f728095
Le Vivilet <levivilet@pm.me>
2025-12-15 22:10:14 +0100

    fix: renderer worker command ids

--- packages/iframe-worker/src/parts/RendererWorker/RendererWorker.ts
+++ packages/iframe-worker/src/parts/RendererWorker/RendererWorker.ts
@@ -1,13 +1,13 @@
-import { RendererWorker as Rpc } from '@lvce-editor/rpc-registry'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const invoke = async (method: string, ...params: readonly any[]): Promise<any> => {
   // @ts-ignore
-  return Rpc.invoke('WebView.compatRendererWorkerInvoke', method, ...params)
+  return RendererWorker.invoke(method, ...params)
 }
 
 export const invokeAndTransfer = async (method: string, ...params: readonly any[]): Promise<any> => {
   // @ts-ignore
-  return Rpc.invokeAndTransfer('WebView.compatRendererWorkerInvokeAndTransfer', method, ...params)
+  return RendererWorker.invokeAndTransfer(method, ...params)
 }
 
 export { RendererWorker } from '@lvce-editor/rpc-registry'
