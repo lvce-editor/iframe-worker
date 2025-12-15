7e6009dac900af78596bbcd1fcb0d0806077061a
Le Vivilet <levivilet@pm.me>
2025-12-15 22:11:45 +0100

    feature: add validation (#357)

--- packages/iframe-worker/src/parts/HandleMessagePort/HandleMessagePort.ts
+++ packages/iframe-worker/src/parts/HandleMessagePort/HandleMessagePort.ts
@@ -1,7 +1,9 @@
+import * as Assert from '@lvce-editor/assert'
 import { PlainMessagePortRpcParent } from '@lvce-editor/rpc'
 import * as RpcRegistry from '../RpcRegistry/RpcRegistry.ts'
 
 export const handleMessagePort2 = async (port: MessagePort, rpcId?: number): Promise<void> => {
+  Assert.object(port)
   const rpc = await PlainMessagePortRpcParent.create({
     commandMap: {},
     messagePort: port,
