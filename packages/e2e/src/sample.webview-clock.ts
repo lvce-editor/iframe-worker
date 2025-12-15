f8866a3ecebed4dabe49a3d40632875009a97cc4
Le Vivilet <levivilet@pm.me>
2025-12-15 22:47:20 +0100

    skip for now

--- packages/e2e/src/sample.webview-clock.ts
+++ packages/e2e/src/sample.webview-clock.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'sample.webview-clock'
 
+export const skip = 1
+
 export const test: Test = async ({ Extension, Main }) => {
   // TODO open sample webview
   // TODO verify that expected content is displayed
--- packages/e2e/src/sample.webview-provider-typo.ts
+++ packages/e2e/src/sample.webview-provider-typo.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'sample.webview-provider-typo'
 
+export const skip = 1
+
 export const test: Test = async ({ expect, Extension, FileSystem, Locator, Main }) => {
   // arrange
   await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
