56e1f9201307ffa882a3d61050862d9b29ef58a8
Le Vivilet <levivilet@pm.me>
2026-01-31 22:49:11 +0100

    feature: update IframeState interface to include assetDir and platform properties (#384)

--- packages/iframe-worker/src/parts/IframeState/IframeState.ts
+++ packages/iframe-worker/src/parts/IframeState/IframeState.ts
@@ -1,15 +1,18 @@
 export interface IframeState {
+  readonly assetDir: string
   readonly credentialless: boolean
   readonly csp: string
   readonly height: number
   readonly id: number
   readonly iframeSrc: string
   readonly origin: string
+  readonly platform: number
   readonly portId: number
   readonly previewServerId: number
   readonly sandbox: string[]
   readonly srcDoc: string
   readonly uri: string
+  readonly webViewScheme: string
   readonly width: number
   readonly x: number
   readonly y: number
