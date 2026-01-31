6a325e37fab6ca385b99593a021a0fa84e10bd88
Le Vivilet <levivilet@pm.me>
2026-01-31 22:03:22 +0100

    fix

--- packages/iframe-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/iframe-worker/src/parts/LoadContent/LoadContent.ts
@@ -1,4 +1,4 @@
-import type { ExplorerState } from '../ExplorerState/ExplorerState.ts'
+import type { IframeState } from '../IframeState/IframeState.ts'
 import * as FileSystem from '../FileSystem/FileSystem.ts'
 import * as GetFileDecorations from '../GetFileDecorations/GetFileDecorations.ts'
 import { getScheme } from '../GetScheme/GetScheme.ts'
@@ -6,7 +6,7 @@ import * as GetSettings from '../GetSettings/GetSettings.ts'
 import * as GetWorkspacePath from '../GetWorkspacePath/GetWorkspacePath.ts'
 import * as RestoreExpandedState from '../RestoreExpandedState/RestoreExpandedState.ts'
 
-export const loadContent = async (state: ExplorerState, savedState: any): Promise<ExplorerState> => {
+export const loadContent = async (state: IframeState, savedState: any): Promise<IframeState> => {
   const { assetDir, platform } = state
   const { confirmDelete, sourceControlDecorations, useChevrons } = await GetSettings.getSettings()
   const workspacePath = await GetWorkspacePath.getWorkspacePath()
