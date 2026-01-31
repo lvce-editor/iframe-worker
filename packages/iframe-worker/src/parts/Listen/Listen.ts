c31b0d0d490de84b881ad797f2b06bb90ab0a9b0
Le Vivilet <levivilet@pm.me>
2026-01-31 22:53:39 +0100

    Discard changes to packages/iframe-worker/src/parts/Listen/Listen.ts

--- packages/iframe-worker/src/parts/Listen/Listen.ts
+++ packages/iframe-worker/src/parts/Listen/Listen.ts
@@ -1,6 +1,5 @@
-import { initializeExtensionHost } from '../InitializeExtensionHost/InitializeExtensionHost.ts'
 import { initializeRendererWorker } from '../InitializeRendererWorker/InitializeRendererWorker.ts'
 
 export const listen = async (): Promise<void> => {
-  await Promise.all([initializeRendererWorker(), initializeExtensionHost()])
+  await initializeRendererWorker()
 }
