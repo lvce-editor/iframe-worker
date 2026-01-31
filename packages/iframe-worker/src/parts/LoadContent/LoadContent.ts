4af3e9a3afa8cbab66e98eb7275986849c7b74f5
Le Vivilet <levivilet@pm.me>
2026-01-31 22:14:59 +0100

    fix

--- packages/iframe-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/iframe-worker/src/parts/LoadContent/LoadContent.ts
@@ -1,6 +1,7 @@
 import type { IframeState } from '../IframeState/IframeState.ts'
 
 export const loadContent = async (state: IframeState, savedState: any): Promise<IframeState> => {
+  // TODO load webview props and register protocol
   return {
     ...state,
   }
