a147565e5f100e04c8efb4d3db80f9cbd05b1f91
Le Vivilet <levivilet@pm.me>
2026-01-31 22:03:51 +0100

    fix

--- packages/iframe-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/iframe-worker/src/parts/LoadContent/LoadContent.ts
@@ -1,27 +1,7 @@
 import type { IframeState } from '../IframeState/IframeState.ts'
-import * as FileSystem from '../FileSystem/FileSystem.ts'
-import * as GetFileDecorations from '../GetFileDecorations/GetFileDecorations.ts'
-import { getScheme } from '../GetScheme/GetScheme.ts'
-import * as GetSettings from '../GetSettings/GetSettings.ts'
-import * as GetWorkspacePath from '../GetWorkspacePath/GetWorkspacePath.ts'
-import * as RestoreExpandedState from '../RestoreExpandedState/RestoreExpandedState.ts'
 
 export const loadContent = async (state: IframeState, savedState: any): Promise<IframeState> => {
-  const { assetDir, platform } = state
-  const { confirmDelete, sourceControlDecorations, useChevrons } = await GetSettings.getSettings()
-  const workspacePath = await GetWorkspacePath.getWorkspacePath()
   return {
     ...state,
-    confirmDelete,
-    decorations,
-    deltaY,
-    excluded,
-    initial: true,
-    items: restoredDirents,
-    maxIndent: 10,
-    minLineY,
-    pathSeparator,
-    root,
-    useChevrons,
   }
 }
