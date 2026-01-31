5bf68ca153085eb446f1fcc1b74c364bfe6273a4
Le Vivilet <levivilet@pm.me>
2026-01-31 22:51:37 +0100

    typo

--- packages/iframe-worker/src/parts/InitializeExtensionHost/InitializeExtensionHost.ts
+++ packages/iframe-worker/src/parts/InitializeExtensionHost/InitializeExtensionHost.ts
@@ -6,7 +6,7 @@ const send = async (port: MessagePort): Promise<void> => {
   await RendererWorker.sendMessagePortToExtensionHostWorker(port, 0)
 }
 
-export const initializExtensionHost = async (): Promise<void> => {
+export const initializeExtensionHost = async (): Promise<void> => {
   const rpc = await LazyTransferMessagePortRpcParent.create({
     commandMap: CommandMap.commandMap,
     send,
