af52360197e8e94713d48a7121df3625bf792bea
Le Vivilet <levivilet@pm.me>
2026-01-31 22:10:27 +0100

    feature: add IframeState interface (#371)

---
+++ packages/iframe-worker/src/parts/IframeState/IframeState.ts
@@ -0,0 +1,16 @@
+export interface IframeState {
+  readonly credentialless: boolean
+  readonly csp: string
+  readonly height: number
+  readonly id: string
+  readonly iframeSrc: string
+  readonly origin: string
+  readonly portId: number
+  readonly previewServerId: number
+  readonly sandbox: string[]
+  readonly srcDoc: string
+  readonly uri: string
+  readonly width: number
+  readonly x: number
+  readonly y: number
+}
