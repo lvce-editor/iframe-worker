e2a00a61f09c9529555b0cf53eb169ef8afcedbe
Le Vivilet <levivilet@pm.me>
2026-02-01 09:52:29 +0100

    move

--- packages/iframe-worker/src/parts/RenderMove/RenderMove.ts
+++ packages/iframe-worker/src/parts/RenderMove/RenderMove.ts
@@ -1,24 +1,6 @@
-import { ViewletCommand } from '@lvce-editor/constants'
-import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { IframeState } from '../IframeState/IframeState.ts'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 
 export const renderMove = (oldState: IframeState, newState: IframeState): any => {
-  const { id, iframeSrc, iframeTitle } = newState
-  // TODO add command to move iframe to correct parent node
-  const dom = [
-    {
-      childCount: 1,
-      type: VirtualDomElements.Div,
-    },
-    {
-      childCount: 0,
-      onLoad: DomEventListenerFunctions.HandleIframeLoad,
-      src: iframeSrc,
-      title: iframeTitle,
-      // @ts-ignore
-      type: VirtualDomElements.Iframe,
-    },
-  ]
-  return [ViewletCommand.SetDom2, id, dom]
+  const { id } = newState
+  return ['Viewlet.move', id, 'iframe', '.iframe-wrapper']
 }
