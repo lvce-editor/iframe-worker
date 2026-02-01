98f4daee283281ee21c2a125bb8600fda290afbd
Le Vivilet <levivilet@pm.me>
2026-02-01 11:36:13 +0100

    fix

--- packages/iframe-worker/src/parts/RenderItems/RenderItems.ts
+++ packages/iframe-worker/src/parts/RenderItems/RenderItems.ts
@@ -10,14 +10,6 @@ export const renderItems = (oldState: IframeState, newState: IframeState): any =
       className: 'iframe-wrapper',
       type: VirtualDomElements.Div,
     },
-    // {
-    //   childCount: 0,
-    //   onLoad: DomEventListenerFunctions.HandleIframeLoad,
-    //   src: iframeSrc,
-    //   title: iframeTitle,
-    //   // @ts-ignore
-    //   type: VirtualDomElements.Iframe,
-    // },
   ]
   return [ViewletCommand.SetDom2, id, dom]
 }
