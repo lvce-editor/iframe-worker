7f693551fe8c850a99081c3b98f201e31882085e
Le Vivilet <levivilet@pm.me>
2026-02-01 14:58:33 +0100

    feature: update renderMove function to use dynamic source and target selectors

--- packages/iframe-worker/src/parts/RenderMove/RenderMove.ts
+++ packages/iframe-worker/src/parts/RenderMove/RenderMove.ts
@@ -2,5 +2,7 @@ import type { IframeState } from '../IframeState/IframeState.ts'
 
 export const renderMove = (oldState: IframeState, newState: IframeState): any => {
   const { id } = newState
-  return ['Viewlet.move', id, 'iframe', '.iframe-wrapper']
+  const source = `.Workbench > iframe`
+  const target = `[data-id="${id}"] .iframe-wrapper`
+  return ['Viewlet.move', id, source, target]
 }
