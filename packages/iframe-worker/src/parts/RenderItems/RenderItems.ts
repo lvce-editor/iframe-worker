9a56a0070d25190f33b5af52112fa6c36c8ec0a5
Le Vivilet <levivilet@pm.me>
2026-02-01 11:36:06 +0100

    fix

--- packages/iframe-worker/src/parts/RenderItems/RenderItems.ts
+++ packages/iframe-worker/src/parts/RenderItems/RenderItems.ts
@@ -1,24 +1,23 @@
 import { ViewletCommand } from '@lvce-editor/constants'
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { IframeState } from '../IframeState/IframeState.ts'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 
 export const renderItems = (oldState: IframeState, newState: IframeState): any => {
-  const { id, iframeSrc, iframeTitle } = newState
+  const { id } = newState
   const dom = [
     {
-      childCount: 1,
+      childCount: 0,
       className: 'iframe-wrapper',
       type: VirtualDomElements.Div,
     },
-    {
-      childCount: 0,
-      onLoad: DomEventListenerFunctions.HandleIframeLoad,
-      src: iframeSrc,
-      title: iframeTitle,
-      // @ts-ignore
-      type: VirtualDomElements.Iframe,
-    },
+    // {
+    //   childCount: 0,
+    //   onLoad: DomEventListenerFunctions.HandleIframeLoad,
+    //   src: iframeSrc,
+    //   title: iframeTitle,
+    //   // @ts-ignore
+    //   type: VirtualDomElements.Iframe,
+    // },
   ]
   return [ViewletCommand.SetDom2, id, dom]
 }
