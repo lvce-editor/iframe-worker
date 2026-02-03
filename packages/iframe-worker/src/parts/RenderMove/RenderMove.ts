798b5264f7bab3069c8dfafd2daf054a3cfd0260
Le Vivilet <levivilet@pm.me>
2026-02-03 23:39:29 +0100

    fix: iframe selector

--- packages/iframe-worker/src/parts/RenderMove/RenderMove.ts
+++ packages/iframe-worker/src/parts/RenderMove/RenderMove.ts
@@ -3,6 +3,6 @@ import type { IframeState } from '../IframeState/IframeState.ts'
 export const renderMove = (oldState: IframeState, newState: IframeState): any => {
   const { id } = newState
   const source = `.Workbench > iframe`
-  const target = `[data-id="${id}"] .iframe-wrapper`
+  const target = `[data-id="${id}"].iframe-wrapper`
   return ['Viewlet.move', id, source, target]
 }
