a8c0f1d2ea70819b4bf43dee2e41e87b96245d95
Le Vivilet <levivilet@pm.me>
2026-09-14 13:21:33 +0200

    feature: expose active editor text to webviews (#426)

--- packages/iframe-worker/src/parts/WebViewRpcCommandMap/WebViewRpcCommandMap.ts
+++ packages/iframe-worker/src/parts/WebViewRpcCommandMap/WebViewRpcCommandMap.ts
@@ -1,7 +1,9 @@
 import * as FileSystem from '../FileSystem/FileSystem.ts'
 import * as GetRemoteUrl from '../GetRemoteUrl/GetRemoteUrl.ts'
+import * as GetTextDocument from '../GetTextDocument/GetTextDocument.ts'
 
 export const commandMap = {
   'WebView.getRemoteUrl': GetRemoteUrl.getRemoteUrl,
+  'WebView.getTextDocument': GetTextDocument.getTextDocument,
   'WebView.readFile': FileSystem.readFile,
 }
